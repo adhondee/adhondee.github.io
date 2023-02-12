cff747917637774a85a7cf658822439ada82ba28
adhondee <adhondee@hotmail.com>
2023-02-12 10:55:09 +0400

    comment openPopup

--- main.js
+++ main.js
@@ -3,6 +3,6 @@ function alertFunction() {
 }
 
 function openPopup(){
-   let newWin = window.open("about:blank", "hello", "width=200,height=200");
-   newWin.document.write("<script>window.opener.document.body.innerHTML = 'Test'<\/script>");
+   //let newWin = window.open("about:blank", "hello", "width=200,height=200");
+   //newWin.document.write("<script>window.opener.document.body.innerHTML = 'Test'<\/script>");
 }
