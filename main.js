4a01263feb2cb92bb2b66c97968c4e4ff0f352bd
adhondee <adhondee@hotmail.com>
2023-02-12 10:29:27 +0400

    added openPopup()

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
 function alertFunction() {
    alert("Hi world! I am an alert box!");
 }
+
+function openPopup(){
+   let newWin = window.open("about:blank", "hello", "width=200,height=200");
+
+   newWin.document.write(
+     "<script>
+         window.opener.document.body.innerHTML = 'Test'
+      <\/script>"
+   );
+}
