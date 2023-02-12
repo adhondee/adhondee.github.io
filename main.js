7d699a8c9b32ee0b9ce33810884ac0491534902d
adhondee <adhondee@hotmail.com>
2023-02-12 10:37:48 +0400

    Update main.js

--- main.js
+++ main.js
@@ -4,10 +4,5 @@ function alertFunction() {
 
 function openPopup(){
    let newWin = window.open("about:blank", "hello", "width=200,height=200");
-
-   newWin.document.write(
-     "<script>
-         window.opener.document.body.innerHTML = 'Test'
-      <\/script>"
-   );
+   newWin.document.write("<script>window.opener.document.body.innerHTML = 'Test'<\/script>");
 }
